7163dfbc0012c1355b3c78d0ca030992ad221d72
abinapier <abi.napier@yahoo.com>
2020-06-20 14:02:25 -0600

    mainActivity design complete. goal preview fragment added

---
+++ app/src/main/java/com/example/trakk/ui/main/GoalPreviewFragment.java
@@ -0,0 +1,22 @@
+package com.example.trakk.ui.main;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.trakk.R;
+
+public class GoalPreviewFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_goal_preview, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/trakk/ui/main/GoalPreviewPresenter.java
@@ -0,0 +1,4 @@
+package com.example.trakk.ui.main;
+
+public class GoalPreviewPresenter {
+}
