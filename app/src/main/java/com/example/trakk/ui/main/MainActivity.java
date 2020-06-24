3b2856ee416d6fb13d648011fce48b7594317bb0
abinapier <abi.napier@yahoo.com>
2020-06-24 09:46:58 -0600

    Main Presenter thread added. Updates ui if there is no file found.

--- app/src/main/java/com/example/trakk/ui/main/MainActivity.java
+++ app/src/main/java/com/example/trakk/ui/main/MainActivity.java
@@ -2,20 +2,31 @@ package com.example.trakk.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.trakk.R;
 
+import java.lang.ref.WeakReference;
+
 public class MainActivity extends AppCompatActivity {
+    private MainPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MainPresenter presenter = new MainPresenter(this);
+        presenter = new MainPresenter(new WeakReference<MainActivity>(this));
+        Thread presenterThread = new Thread(presenter);
+        presenterThread.start();
     }
 
-    public void updateProgress(){
+    public void addGoal(View view){
+
+    }
+
+    public void viewGoalDetail(View view){
 
     }
 }
--- app/src/main/java/com/example/trakk/ui/main/MainPresenter.java
+++ app/src/main/java/com/example/trakk/ui/main/MainPresenter.java
@@ -1,13 +1,57 @@
 package com.example.trakk.ui.main;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.trakk.R;
 import com.example.trakk.model.FileHelper;
 import com.example.trakk.model.User;
 
-public class MainPresenter {
+import java.lang.ref.WeakReference;
+
+public class MainPresenter implements Runnable{
+
+    private static final String TAG = "Main Presenter:";
     public User user;
-    private MainActivity ui;
-    public MainPresenter(MainActivity ui){
+    private WeakReference<MainActivity> ui;
+    public MainPresenter(WeakReference<MainActivity> ui){
         user = new User();
         this.ui = ui;
+
+    }
+
+
+    @Override
+    public void run(){
+
+        if(ui.get() != null) {
+
+
+            ui.get().runOnUiThread(new Runnable() {
+                public void run() {
+                    Context context = ui.get().getApplicationContext();
+                    LinearLayout layout = ui.get().findViewById(R.id.goalPreviewContainer);
+                    if(FileHelper.fileExists(context.getFilesDir().toString())){
+                        //file exists, load file and add goal preview fragments
+                        Log.d(TAG, "MainPresenter: File Exists");
+                    }else{
+                        //file doesn't exist, add message to view
+                        Log.d(TAG, "MainPresenter: File Does not Exist");
+
+                        TextView textView = new TextView(ui.get());
+                        textView.setTextSize(25);
+                        textView.setText("No goals found.");
+                        textView.setGravity(Gravity.CENTER);
+                        layout.addView(textView);
+                        layout.invalidate();
+                    }
+
+                }
+            });
+        }
     }
 }
