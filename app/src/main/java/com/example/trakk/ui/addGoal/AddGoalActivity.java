53489dd822ccf3d2af11e8c1908f0207f10a490a
eduardourbiola <urb17002@byui.edu>
2020-06-23 22:26:20 -0600

    Link Main Activity with Add New Goal

--- app/src/main/java/com/example/trakk/ui/addGoal/AddGoalActivity.java
+++ app/src/main/java/com/example/trakk/ui/addGoal/AddGoalActivity.java
@@ -2,14 +2,82 @@ package com.example.trakk.ui.addGoal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.trakk.R;
 
-public class AddGoalActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.time.DayOfWeek;
+import java.util.Calendar;
+
+public class AddGoalActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private static final String TAG = "AddGoalActivity";
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener onDateSetListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_goal);
+        Spinner aSpinner = findViewById(R.id.aSpinner);
+        aSpinner.setOnItemSelectedListener(this);
+
+
+        mDisplayDate = (TextView) findViewById(R.id.tvDate);
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(AddGoalActivity.this, android.R.style.Theme_DeviceDefault_Light,
+                        onDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                dialog.show();
+
+
+            }
+        });
+
+
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yy: " + month + "/" + dayOfMonth + "/" + year);
+                String date = month + "/" + dayOfMonth + "/" + year;
+                mDisplayDate.setText(date);
+
+            }
+        };
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(this, adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView){
+
     }
 }
+
--- app/src/main/java/com/example/trakk/ui/main/MainActivity.java
+++ app/src/main/java/com/example/trakk/ui/main/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.trakk.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.trakk.R;
+import com.example.trakk.ui.addGoal.AddGoalActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateProgress(){
 
+    }
+    public void addGoalButon(View view){
+        Intent intent = new Intent(MainActivity.this, AddGoalActivity.class);
+        startActivity(intent);
     }
 }
