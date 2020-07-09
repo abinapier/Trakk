f88618b1ff3c0d163c19b4e288c7396b6ae8255b
Emoiraine <49993780+Emoiraine@users.noreply.github.com>
2020-07-09 05:06:25 -0600

    trying to use an onBind method to combine the data and attach it to the user information so when clicking on a goal the details are there and attached to itself.

--- app/src/main/java/com/example/trakk/ui/goalDetail/GoalDetailPresenter.java
+++ app/src/main/java/com/example/trakk/ui/goalDetail/GoalDetailPresenter.java
@@ -1,4 +1,16 @@
 package com.example.trakk.ui.goalDetail;
 
+import android.os.Binder;
+import android.os.IBinder;
+import com.example.trakk.model.User;
+
 public class GoalDetailPresenter {
+
+    onBind(usrString){
+
+        GoalDetailPresenter.user;
+    }
+
+    public interface View {
+    }
 }
