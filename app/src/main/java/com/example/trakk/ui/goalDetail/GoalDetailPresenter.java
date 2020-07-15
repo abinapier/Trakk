33ffd7f54303cd2d7383bfa3b4f8e5f3f1322513
Emoiraine <49993780+Emoiraine@users.noreply.github.com>
2020-07-15 15:53:21 -0600

    This was my understanding of what we wanted to do. It doesn't fully work. I'm not sure how to make the loop work and then set it properly. I tried to do what we talked about.

--- app/src/main/java/com/example/trakk/ui/goalDetail/GoalDetailPresenter.java
+++ app/src/main/java/com/example/trakk/ui/goalDetail/GoalDetailPresenter.java
@@ -1,17 +1,70 @@
 package com.example.trakk.ui.goalDetail;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.trakk.R;
 import com.example.trakk.model.FileHelper;
+import com.example.trakk.model.Goals;
 import com.example.trakk.model.User;
 
-public class GoalDetailPresenter {
-    private User userClass;
-    GoalDetailPresenter(String goalName){
-        //use the file helper to load the user class
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+
+import static android.content.ContentValues.TAG;
+
+public class GoalDetailPresenter implements Runnable {
+    private final String goalName;
+    private User user;
+    private WeakReference<GoalDetailActivity> ui;
+
+    GoalDetailPresenter(String goalName) {
+
+        //I want to use Filehelper to load the user class.
+        //I want to say if goalName is found then load goal name
+        //when GoalDetail is pulled up
+
+    }
 
 
+    @Override
+    public void run(){
+
+        if(ui.get() != null) {
+
+
+            ui.get().runOnUiThread(new Runnable() {
+                public void run() {
+                    Context context = ui.get().getApplicationContext();
+                    LinearLayout layout = ui.get().findViewById(R.id.goalPreviewContainer);
+                    if (FileHelper.fileExists(context.getFilesDir().toString())) {
+                        //file exists, load file
+                        Log.d(TAG, "GoalDetailPresenter: File Exists");
+                        try {
+                            //get user object from file
+                            user = FileHelper.ReadFile(context.getFilesDir().toString());
+
+                            for (int i = 0; i < goalNum; i++) {
+
+                                String goalName = user.getGoals();
+                                goalName.setText(to goal name);
+
+                            }
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
     }
 
 
     public interface View {
+
     }
-}
+    }
+
+
