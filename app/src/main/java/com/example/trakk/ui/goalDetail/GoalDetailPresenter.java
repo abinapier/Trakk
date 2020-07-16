8aa7dd037d8f7d76b316fa69f2cd66fc88c9ecf0
abinapier <abi.napier@yahoo.com>
2020-07-16 10:26:39 -0600

    start of update

--- app/src/main/java/com/example/trakk/ui/goalDetail/GoalDetailPresenter.java
+++ app/src/main/java/com/example/trakk/ui/goalDetail/GoalDetailPresenter.java
@@ -12,6 +12,7 @@ import com.example.trakk.model.User;
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.util.List;
 
 import static android.content.ContentValues.TAG;
 
@@ -25,7 +26,7 @@ public class GoalDetailPresenter implements Runnable {
         //I want to use Filehelper to load the user class.
         //I want to say if goalName is found then load goal name
         //when GoalDetail is pulled up
-
+        this.goalName = goalName;
     }
 
 
@@ -38,18 +39,21 @@ public class GoalDetailPresenter implements Runnable {
             ui.get().runOnUiThread(new Runnable() {
                 public void run() {
                     Context context = ui.get().getApplicationContext();
-                    LinearLayout layout = ui.get().findViewById(R.id.goalPreviewContainer);
                     if (FileHelper.fileExists(context.getFilesDir().toString())) {
                         //file exists, load file
                         Log.d(TAG, "GoalDetailPresenter: File Exists");
                         try {
                             //get user object from file
                             user = FileHelper.ReadFile(context.getFilesDir().toString());
-
-                            for (int i = 0; i < goalNum; i++) {
-
-                                String goalName = user.getGoals();
-                                goalName.setText(to goal name);
+                            List<Goals> myGoals = user.getGoals();
+                            for (int i = 0; i < myGoals.length(); i++) {
+                                Goals curGoal = myGoals.get(i);
+                                if(curGoal.getGoalName() == goalName){
+                                    //set the ui values
+                                    TextView goalNameNew = ui.get().findViewById(R.id.goalName);
+                                    goalNameNew.setText(curGoal.getGoalName());
+                                    break;
+                                }
 
                             }
                         } catch (IOException ex) {
