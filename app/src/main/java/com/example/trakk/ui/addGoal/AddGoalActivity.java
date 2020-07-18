c102f9e93c0a3d9844a9f08fe68c5731c9c3807e
abinapier <abi.napier@yahoo.com>
2020-07-17 23:11:49 -0600

    add goal saving goals

--- app/src/main/java/com/example/trakk/ui/addGoal/AddGoalActivity.java
+++ app/src/main/java/com/example/trakk/ui/addGoal/AddGoalActivity.java
@@ -8,17 +8,25 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.trakk.R;
+import com.example.trakk.model.FileHelper;
+import com.example.trakk.model.Goals;
+import com.example.trakk.model.Subtask;
+import com.example.trakk.model.User;
+import com.example.trakk.model.frequency;
 import com.example.trakk.ui.addSubtask.AddSubtaskActivity;
 import com.example.trakk.ui.main.MainActivity;
 
@@ -26,8 +34,11 @@ import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
 import java.util.Calendar;
+import java.util.Date;
 
 public class AddGoalActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private static final String TAG = "AddGoalActivity";
@@ -96,14 +107,30 @@ public class AddGoalActivity extends AppCompatActivity implements AdapterView.On
         Intent intent = new Intent(this, AddSubtaskActivity.class);
         startActivity(intent);
     }
-    public void editGoalButton(View view){
+    public void editGoalButton(View view) throws ParseException, IOException {
+        TextView NameGoal =  findViewById(R.id.GoalName);
+        TextView GoalDescription = findViewById(R.id.GoalDescription);
+        TextView startDateText = findViewById(R.id.tvDate);
+        Date startDate = new SimpleDateFormat("dd/MM/yyyy").parse(startDateText.getText().toString());
+        Spinner goalFrequency = findViewById(R.id.aSpinner);
+        frequency curFrequency = frequency.valueOf(goalFrequency.getSelectedItem().toString());
+        Goals curGoal = new Goals(NameGoal.getText().toString(), GoalDescription.getText().toString(), startDate, curFrequency);
+        LinearLayout subtaskLayout = findViewById(R.id.containerTask);
+        for (int i = 0; i < subtaskLayout.getChildCount(); i++) {
+            ViewGroup v = (ViewGroup) subtaskLayout.getChildAt(i);
+            ViewGroup layerOne = (ViewGroup) v.getChildAt(1);
+            ViewGroup text = (ViewGroup) layerOne.getChildAt(1);
+            TextView subTaskName = (TextView) text.getChildAt(0);
+            TextView subTaskDescription = (TextView) text.getChildAt(1);
+            Subtask curSubtask = new Subtask(subTaskDescription.getText().toString(), subTaskName.getText().toString());
+            curGoal.addTask(curSubtask);
+        }
+
+        User fileUser = FileHelper.ReadFile(this.getApplicationContext().getFilesDir().toString());
+        fileUser.addGoal(curGoal);
+        FileHelper.WriteFile(fileUser, this.getApplicationContext().getFilesDir().toString());
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-        String NameGoal =  findViewById(R.id.GoalName).toString();
-        String GoalDescription = findViewById(R.id.GoalDescription).toString();
-        Bundle NewGoal = new Bundle();
-        NewGoal.putString("GoalName", NameGoal);
-        NewGoal.putString("GoalDescription", GoalDescription);
     }
 
 }
